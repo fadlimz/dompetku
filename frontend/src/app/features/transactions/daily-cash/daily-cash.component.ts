c41036be74bd3b32634d726f701c12ca9fe15a8b
fadlimz <zafad@outlook.com>
2026-02-28 23:28:39 +0700

    feat: load accounts and categories for dropdowns

--- frontend/src/app/features/transactions/daily-cash/daily-cash.component.ts
+++ frontend/src/app/features/transactions/daily-cash/daily-cash.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../../../core/services/account.service';
+import { CategoryService } from '../../../core/services/category.service';
+import { Account } from '../../../core/models/account.model';
+import { Category } from '../../../core/models/category.model';
 
 @Component({
   selector: 'app-daily-cash',
@@ -10,5 +14,36 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./daily-cash.component.scss']
 })
 export class DailyCashComponent implements OnInit {
-  ngOnInit(): void {}
+  accounts: Account[] = [];
+  categories: Category[] = [];
+
+  constructor(
+    private accountService: AccountService,
+    private categoryService: CategoryService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadAccounts();
+    this.loadCategories();
+  }
+
+  loadAccounts(): void {
+    this.accountService.getAll().subscribe({
+      next: (data: Account[]) => {
+        this.accounts = data.sort((a: Account, b: Account) =>
+          a.accountName.localeCompare(b.accountName)
+        );
+      }
+    });
+  }
+
+  loadCategories(): void {
+    this.categoryService.getAll().subscribe({
+      next: (data: Category[]) => {
+        this.categories = data
+          .filter((c: Category) => c.activeFlag === 'Active')
+          .sort((a: Category, b: Category) => a.categoryName.localeCompare(b.categoryName));
+      }
+    });
+  }
 }
